f7ea45424151bae81f6bce81effb01470e93f915
hasser0 <osvaldoes.unam@gmail.com>
2021-11-11 17:31:09 -0600

    Implemented dataframe to csv

--- get_faces.py
+++ get_faces.py
@@ -1,15 +1,18 @@
 import cv2
 import os
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime
 ############################################################
 ###############   VARIABLES AND CONSTANTS   ################
 ############################################################
 AREA = 4
-output_size = (254,254)
-r = 1 # radius for black filter
-h_padd = 15
-w_padd = 15
+IMAGE_SIZE = 256
+# radius for black filter
+r = 1 
+h_padd = 30
+w_padd = 30
 lower_area_white = 50
 upper_area_white = 40000
 lower_area_yellow = 100
@@ -20,25 +23,26 @@ connect = 4
 sensitivity_white = 50
 sensitivity_black = 100
 PATH = '/home/hasser/semestre_7/model_gener/simpsons_face_detector/'
-PATH_TO_VIDEOS = PATH + 'videos/'
-output_dir = './simpson_faces/'
+input_videos = PATH + 'videos/videos2'
+output_scenes = PATH + 'scenes/output_scenes2'
+output_boxes = PATH + 'boxes/output_boxes2'
+output_faces = PATH + 'faces/output_faces2'
+csv_file = 'bounds.csv'
 #YELLOW BOUNDS
-lower_yellow = np.array([22, 150, 120], dtype="uint16")
-upper_yellow = np.array([50, 255, 255], dtype="uint16")
-lower_yellow = np.array([22, 93, 0], dtype="uint16")
-upper_yellow = np.array([45, 255, 255], dtype="uint16")
+lower_yellow = np.array([0, 193, 157], dtype="uint16")
+upper_yellow = np.array([30, 255, 255], dtype="uint16")
 #WHITE BOUNDS
 lower_white = np.array([0,0,255-sensitivity_white], dtype="uint16")
 upper_white = np.array([255,sensitivity_white,255], dtype="uint16")
 #BLACK BOUNDS
 lower_black = np.array([0,0,0], dtype="uint16")
 upper_black = np.array([255,255,sensitivity_black], dtype="uint16")
-
+df = pd.DataFrame(columns=['x', 'y', 'width', 'height', 'frame_id', 'face_num', 'video'])
 ############################################################
 ########################   PROGRAM   #######################
 ############################################################
-for video_name in os.listdir(PATH_TO_VIDEOS):
-    cap = cv2.VideoCapture(PATH_TO_VIDEOS + video_name)
+for video_name in os.listdir(input_videos):
+    cap = cv2.VideoCapture(input_videos + video_name)
     frame_rate = 30
     frame_num = 0
     while cap.isOpened():
@@ -116,14 +120,31 @@ for video_name in os.listdir(PATH_TO_VIDEOS):
                     n_white_boxes +=1
             if n_white_boxes == 2:
                 faces.append(yellow_box)
-
-        for face in faces:
+        frame_id = datetime.now().strftime("%Y%m%d%H%M%S%f")
+        for face_num, face in enumerate(faces):
             x,y,w,h,a = face
             try:
                 cropped_image = image[y-h_padd : y+h+h_padd, x-w_padd : x+w+w_padd]
+                box = {'x':x-w_padd,
+                    'y':y-h_padd,
+                    'width':w + 2*w_padd,
+                    'height':h + 2*h_padd,
+                    'frame_id':frame_id,
+                    'face_num':face_num,
+                    'video':video_name}
             except IndexError:
                 cropped_image = image[y:y+h,x:x+w]
+                box = {'x':x,
+                    'y':y,
+                    'width':w,
+                    'height':h,
+                    'frame_id':frame_id,
+                    'face_num':face_num,
+                    'video':video_name}
             finally:
-                cropped_image = cv2.resize(cropped_image, output_size)
-                cv2.imwrite(output_dir + video_name + f'_{frame_num}.png', cropped_image)
-        print(f"Frame {frame_num} finished: {len(faces)} faces found")
+                df = df.append(box, ignore_index=True)
+                cropped_image = cv2.resize(cropped_image, (IMAGE_SIZE, IMAGE_SIZE))
+                cv2.imwrite(f"{output_faces}{frame_id}_{face_num}.png", cropped_image)
+                cv2.imwrite(f"{output_scenes}{frame_id}.png", image)
+    df.to_csv(output_boxes + csv_file)
+    df.to_csv(output_boxes + video_name[:-4] + '_' + csv_file)
